2dac76731f40d97552ee00498d46b6c3ee7de589
Harsh Dhiman <100757151+dhimanharsh17@users.noreply.github.com>
2024-06-19 18:00:04 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Harsh</h1>
+        <p className={styles.description}>
+          I'm a Web Developer with 1 year of experience using HTML, CSS, React
+          MongoDB. Reach out if you'd like to learn more! 
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("about/developerImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
